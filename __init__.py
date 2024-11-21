dba78b6dc094aa7b36fb6d98fa7e142d21947d86
GHEMDANE <127841727+GHEMDANE@users.noreply.github.com>
2024-11-21 15:46:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
     
 if __name == "__main":
   app.run(debug=True)
